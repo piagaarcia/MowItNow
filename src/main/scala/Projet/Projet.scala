ac1ee3787dd7a62f68fd62740ed749968bd71433
Pia Garcia <piagarcia1999@gmail.com>
2022-01-06 19:15:03 +0100

    adding tondeuse

--- src/main/scala/Projet/Projet.scala
+++ src/main/scala/Projet/Projet.scala
@@ -1,5 +1,16 @@
 package Projet
 
-object Projet {
+import scala.io.Source.fromResource
 
+
+class Tondeuse (var coordonnees : Int,
+                var orientation : String)
+
+
+object Projet extends App {
+
+
+  try {
+    val input = fromResource("test.txt").
+  }
 }
