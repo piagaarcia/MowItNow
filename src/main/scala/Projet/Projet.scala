3b001ec65bb2afdf50b85a05e3aedd2ba9746fdc
Pia Garcia <piagarcia1999@gmail.com>
2022-01-08 18:10:57 +0100

    try/catch/case

--- src/main/scala/Projet/Projet.scala
+++ src/main/scala/Projet/Projet.scala
@@ -1,5 +1,6 @@
 package Projet
 
+import java.io.{FileNotFoundException, IOException}
 import scala.io.Source.fromResource
 
 
@@ -9,8 +10,30 @@ class Tondeuse (var coordonnees : Int,
 
 object Projet extends App {
 
-
+/*fichier fourni en entrée*/
   try {
-    val input = fromResource("test.txt").
+    var input = fromResource("test.txt").getLines()
+  } catch {
+    case e: FileNotFoundException => println("Couldn't find that file.")
+    case e: IOException => println("Had an IOException trying to read that file")
   }
+
+/*definir les directions de la tondeuse*/
+    private def Direction {
+      case "N" => Direction.N
+      case "E" => Direction.E
+      case "W" => Direction.W
+      case "S" => Direction.S
+
+      case Direction.N => "N"
+      case Direction.E => "E"
+      case Direction.W => "W"
+      case Direction.S => "S"
+
+
+
+
+    }
+
+  val sequence = Seq(N, E, W, S)
 }
--- src/main/scala/Projet/direction.scala
+++ src/main/scala/Projet/direction.scala
@@ -1,21 +1,9 @@
 package Projet
 
 object direction extends App {
-
-
-
     type Direction = Value
 
     val N, S, E, W = Value
 
-    def parse(s: String) = s match {
-      case "N" => Direction.N
-      case "E" => Direction.E
-      case "W" => Direction.W
-      case "S" => Direction.S
 
-    }
-
-    val sequence = Seq(N, E, W, S)
   }
-}
